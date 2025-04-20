612e0410dee50f77da01ad565474186110c06540
peanutto <129247103+tunykush@users.noreply.github.com>
2025-04-21 01:48:37 +1000

    updated cart

--- products.js
+++ products.js
@@ -1,6 +1,238 @@
 const products = [
     {
         id: 1,
+        title: "Audi ABT RS6-R Glacier White Metallic",
+        price: "350₫",
+        description: "MINI GT 1:64 Audi ABT RS6-R Glacier White Metallic MGT00701",
+        colors: [
+            {
+                name: "White",
+                mainImage: "images/audiwhite2.jpg",
+                thumbnails: [
+                    "images/audiwhite1.jpg",
+                    "images/audiwhite3.jpg",
+                    "images/audiwhite4.jpg"
+                ],
+                sizes: ["1:64"]
+            }
+        ]
+    },
+    {
+        id: 2,
+        title: "Bentley Continental GT Speed Anthracite Satin",
+        price: "550",
+        description: "MINI GT 1:64 Bugatti Centodieci Black MGT00466-L",
+        colors: [
+            {
+                name: "Black",
+                mainImage: "images/bentleysatin1.jpg",
+                thumbnails: [
+                    "images/bentleysatin2.jpg",
+                    "images/bentleysatin3.jpg",
+                    "images/bentleysatin4.jpg"
+                ],
+                sizes: ["1:64"]
+            }
+        ]
+    },
+    {
+        id: 3,
+        title: "Bugatti Centodieci Black",
+        price: "300",
+        description: "MINI GT 1:64 Bugatti Centodieci Black MGT00466-L",
+        colors: [
+            {
+                name: "Black",
+                mainImage: "images/bugatti4661.jpg",
+                thumbnails: [
+                    "images/bugatti4662.jpg",
+                    "images/bugatti4663.jpg",
+                    "images/bugatti4664.jpg"
+                ],
+                sizes: ["S", "M", "L", "XL", "XXL"]
+            }
+        ]
+    },
+    {
+        id: 4,
+        title: "Bugatti Vision Gran Turismo Black",
+        price: "350",
+        description: "MINI GT 1:64 Bugatti Vision Gran Turismo Black MGT00795-L",
+        colors: [
+            {
+                name: "Black",
+                mainImage: "images/bugatti9751.jpg",
+                thumbnails: [
+                    "images/bugatti7952.jpg",
+                    "images/bugatti7953.jpg"
+                ],
+                sizes: ["1:64"]
+            }
+        ]
+    },
+    {
+        id: 5,
+        title: "Lamborghini  LB-Silhouette WORKS Aventador GT EVO  Matte Black",
+        price: "950",
+        description: "MINI GT 1:64 Lamborghini  LB-Silhouette WORKS Aventador GT EVO  Matte Black MGT00502",
+        colors: [
+            {
+                name: "Black",
+                mainImage: "images/lbwkaventador1.jpg",
+                thumbnails: [
+                    "images/lbwkaventador2.jpg",
+                    "images/lbwkaventador3.jpg",
+                    "images/lbwkaventador4.jpg"
+                ],
+                sizes: ["1:64"]
+            }
+        ]
+    },
+    {
+        id: 6,
+        title: "Lamborghini Huracán GT3 EVO #4 2022 Macau GP Macau GT Cup 3rd Place",
+        price: "600",
+        description: "MINI GT 1:64 Lamborghini Huracán GT3 EVO #4 2022 Macau GP Macau GT Cup 3rd Place MGT00645",
+        colors: [
+            {
+                name: "Black",
+                mainImage: "images/lambomacau1.jpg",
+                thumbnails: [
+                    "images/lambomacau2.jpg",
+                    "images/lambomacau3.jpg",
+                    "images/lambomacau4.jpg"
+                ],
+                sizes: ["1:64"]
+            }
+        ]
+    },
+    {
+        id: 7,
+        title: "Lamborghini Huracán GT3 EVO2 #83 Iron Dames 2023 IMSA Daytona 24 Hrs",
+        price: "450",
+        description: "MINI GT 1:64 Lamborghini Huracán GT3 EVO2 #83 Iron Dames 2023 IMSA Daytona 24 Hrs MGT00772",
+        colors: [
+            {
+                name: "Pink",
+                mainImage: "images/lambopink1.jpg",
+                thumbnails: [
+                    "images/lambopink2.jpg",
+                    "images/lambopink3.jpg",
+                    "images/lambopink4.jpg"
+                ],
+                sizes: ["1:64"]
+            }
+        ]
+    },
+    {
+        id: 8,
+        title: "Lamborghini Sian FKP 37 Presentation",
+        price: "300",
+        description: "MINI GT 1:64 Lamborghini Sian FKP 37 Presentation MGT00529-MJ",
+        colors: [
+            {
+                name: "Matte green",
+                mainImage: "images/lambosiangreen1.jpg",
+                thumbnails: [
+                    "images/lambosiangreen2.jpg",
+                    "images/lambosiangreen3.jpg"
+                ],
+                sizes: ["1:64"]
+            }
+        ]
+    },
+    {
+        id: 9,
+        title: "Lamborghini Urus Performante Grigio Nimbus",
+        price: "500",
+        description: "MINI GT 1:64 Lamborghini Urus Performante Grigio Nimbus MGT00747",
+        colors: [
+            {
+                name: "Grey and Black",
+                mainImage: "images/uruslambo1.jpg",
+                thumbnails: [
+                    "images/urusblack2.jpg",
+                    "images/urusblack3.jpg",
+                    "images/urusblack4.jpg"
+                ],
+                sizes: ["1:64"]
+            }
+        ]
+    },
+    {
+        id: 10,
+        title: "LB★WORKS Lamborghini Huracán GT Digital Camouflage",
+        price: "600",
+        description: "MINI GT 1:64 LB★WORKS Lamborghini Huracán GT Digital Camouflage MGT00398",
+        colors: [
+            {
+                name: "Grey",
+                mainImage: "images/lambocamo1.jpg",
+                thumbnails: [
+                    "images/lambocamo2.jpg",
+                    "images/lambocamo3.jpg",
+                    "images/lambocamo4.jpg"
+                ],
+                sizes: ["1:64"]
+            }
+        ]
+    },
+    {
+        id: 11,
+        title: "MAZDA RX-7 LB-Super Silhouette #41 Numero Reserve",
+        price: "320",
+        description: "MINI GT 1:64 MAZDA RX-7 LB-Super Silhouette #41 Numero Reserve MGT00773",
+        colors: [
+            {
+                name: "White",
+                mainImage: "images/mazdalbw1.jpg",
+                thumbnails: [
+                    "images/mazdalbw2.jpg",
+                    "images/mazdalbw3.jpg",
+                    "images/mazdalbw4.jpg"
+                ],
+                sizes: ["1:64"]
+            }
+        ]
+    },
+    {
+        id: 12,
+        title: "McLaren 720S LB★Works Black",
+        price: "320",
+        description: "MINI GT 1:64 McLaren 720S LB★Works Black MGT00777",
+        colors: [
+            {
+                name: "Black",
+                mainImage: "images/laren1.jpg",
+                thumbnails: [
+                    "images/laren2.jpg",
+                    "images/laren3.jpg",
+                    "images/laren4.jpg"
+                ],
+                sizes: ["1:64"]
+            }
+        ]
+    },
+    {
+        id: 13,
+        title: "McLaren Artura Ice Silver",
+        price: "400",
+        description: "MINI GT 1:64 McLaren Artura Ice Silver MGT00582",
+        colors: [
+            {
+                name: "Silver",
+                mainImage: "images/atura1.jpg",
+                thumbnails: [
+                    "images/atura2.jpg",
+                    "images/atura3.jpg",
+                    "images/atura4.jpg"
+                ],
+                sizes: ["1:64"]
+            }
+        ]
+    },
+    {
+        id: 14,
         title: "Porsche 911 (992) GT3 RS Weissach Package White with Pyro Red",
         price: "650₫",
         description: "MINI GT 1:64 Porsche 911 (992) GT3 RS Weissach Package White with Pyro Red MGT00706",
@@ -11,7 +243,7 @@ const products = [
                 thumbnails: [
                     "images/porschered2.jpg",
                     "images/porschered3.jpg",
-                    "images/porschered4.jpg",
+                    "images/porschered4.jpg"
                 ],
                 sizes: ["1:64"]
             },
@@ -58,24 +290,7 @@ const products = [
         ]
     },
     {
-        id: 2,
-        title: "Porsche 911 (992) GT3 Touring Crayon ",
-        price: "370₫",
-        description: "MINI GT 1:64 Porsche 911 (992) GT3 Touring Crayon MGT00675-R",
-        colors: [
-            {
-                name: "Crayon",
-                mainImage: "images/porsche911crayon1.jpg",
-                thumbnails: [
-                    "images/porsche911crayon2.jpg",
-                    "images/porsche911crayon3.jpg",
-                ],
-                sizes: ["1:64"]
-            }
-        ]
-    },
-    {
-        id: 3,
+        id: 15,
         title: "Porsche 911 (992) GT3 Touring Black",
         price: "330₫",
         description: "MINI GT 1:64 Porsche 911 (992) GT3 Touring Black MGT00606-R",
@@ -85,244 +300,27 @@ const products = [
                 mainImage: "images/porsche911black1.jpg",
                 thumbnails: [
                     "images/porsche911black2.jpg",
-                    "images/porsche911black3.jpg",
+                    "images/porsche911black3.jpg"
                 ],
                 sizes: ["1:64"]
             }
         ]
     },
     {
-        id: 4,
-        title: "Audi ABT RS6-R Glacier White Metallic",
-        price: "350₫",
-        description: "MINI GT 1:64 Audi ABT RS6-R Glacier White Metallic MGT00701",
+        id: 16,
+        title: "Porsche 911 (992) GT3 Touring Crayon ",
+        price: "370₫",
+        description: "MINI GT 1:64 Porsche 911 (992) GT3 Touring Crayon MGT00675-R",
         colors: [
             {
-                name: "White",
-                mainImage: "images/audiwhite2.jpg",
-                thumbnails: ["images/audiwhite1.jpg",
-                    "images/audiwhite3.jpg",
-                    "images/audiwhite4.jpg",
-                ],
-                sizes: ["1:64"]
-            }
-        ]
-    },
-    {
-        id: 5,
-        title: "Lamborghini  LB-Silhouette WORKS Aventador GT EVO  Matte Black",
-        price: "950",
-        description: "MINI GT 1:64 Lamborghini  LB-Silhouette WORKS Aventador GT EVO  Matte Black MGT00502",
-        colors: [
-            {
-                name: "Black",
-                mainImage: "images/lbwkaventador1.jpg",
+                name: "Crayon",
+                mainImage: "images/porsche911crayon1.jpg",
                 thumbnails: [
-                    "images/lbwkaventador2.jpg",
-                    "images/lbwkaventador3.jpg",
-                    "images/lbwkaventador4.jpg",
+                    "images/porsche911crayon2.jpg",
+                    "images/porsche911crayon3.jpg"
                 ],
                 sizes: ["1:64"]
             }
         ]
-    },
-    {
-        id: 6,
-        title: "Bugatti Vision Gran Turismo Black",
-        price: "350",
-        description:"MINI GT 1:64 Bugatti Vision Gran Turismo Black MGT00795-L",
-        colors: [
-            {
-                name: "Black",
-                mainImage: "images/bugatti9751.jpg",
-                thumbnails: [
-                    "images/bugatti7952.jpg",
-                    "images/bugatti7953.jpg",
-                ],
-                sizes: ["1:64"]
-            }
-        ]
-    },
-    {
-        id: 7,
-        title: "Bugatti Centodieci Black",
-        price: "300",
-        description: "	MINI GT 1:64 Bugatti Centodieci Black MGT00466-L",
-        colors: [
-            {
-                name: "Black",
-                mainImage: "images/bugatti4661.jpg",
-                thumbnails: [
-                    "images/bugatti4662.jpg",
-                    "images/bugatti4663.jpg",
-                    "images/bugatti4664.jpg",
-                ],
-                sizes: ["S", "M", "L", "XL", "XXL"]
-            }
-        ]
-    },
-    {
-        id: 8,
-        title: "Bentley Continental GT Speed Anthracite Satin",
-        price: "550",
-        description: "MINI GT 1:64 Bugatti Centodieci Black MGT00466-L",
-        colors: [
-            {
-                name: "Black",
-                mainImage: "images/bentleysatin1.jpg",
-                thumbnails: [
-                    "images/bentleysatin2.jpg",
-                    "images/bentleysatin3.jpg",
-                    "images/bentleysatin4.jpg",
-
-                ],
-                sizes: ["1:64"]
-            }
-        ]
-    },
-    {
-        id: 9,
-        title: "MAZDA RX-7 LB-Super Silhouette #41 Numero Reserve",
-        price: "320",
-        description: "MINI GT 1:64 MAZDA RX-7 LB-Super Silhouette #41 Numero Reserve MGT00773",
-        colors: [
-            {
-                name: "White",
-                mainImage: "images/mazdalbw1.jpg",
-                thumbnails: [
-                    "images/mazdalbw2.jpg",
-                    "images/mazdalbw3.jpg",
-                    "images/mazdalbw4.jpg"
-                ],
-                sizes: ["1:64"]
-            }
-        ]
-    },
-    {
-        id: 10,
-        title: "McLaren 720S LB★Works Black",
-        price: "320",
-        description: "MINI GT 1:64 McLaren 720S LB★Works Black MGT00777",
-        colors: [
-            {
-                name: "Black",
-                mainImage: "images/laren1.jpg",
-                thumbnails: [
-                    "images/laren2.jpg",
-                    "images/laren3.jpg",
-                    "images/laren4.jpg"
-                ],
-                sizes: ["1:64"]
-            }
-        ]
-    },
-    {
-        id: 11,
-        title: "Lamborghini Sian FKP 37 Presentation",
-        price: "300",
-        description: "MINI GT 1:64 Lamborghini Sian FKP 37 Presentation MGT00529-MJ",
-        colors: [
-            {
-                name: "Matte green",
-                mainImage: "images/lambosiangreen1.jpg",
-                thumbnails: [
-                    "images/lambosiangreen2.jpg",
-                    "images/lambosiangreen3.jpg"
-                ],
-                sizes: ["1:64"]
-            }
-        ]
-    },
-    {
-        id: 12,
-        title: "LB★WORKS Lamborghini Huracán GT Digital Camouflage",
-        price: "600",
-        description: "MINI GT 1:64 LB★WORKS Lamborghini Huracán GT Digital Camouflage MGT00398",
-        colors: [
-            {
-                name: "Grey",
-                mainImage: "images/lambocamo1.jpg",
-                thumbnails: [
-                    "images/lambocamo2.jpg",
-                    "images/lambocamo3.jpg",
-                    "images/lambocamo4.jpg"
-                ],
-                sizes: ["1:64"]
-            }
-        ]
-    },
-    {
-        id: 13,
-        title: "Lamborghini Huracán GT3 EVO #4 2022 Macau GP Macau GT Cup 3rd Place",
-        price: "600",
-        description: "MINI GT 1:64 Lamborghini Huracán GT3 EVO #4 2022 Macau GP Macau GT Cup 3rd Place MGT00645",
-        colors: [
-            {
-                name: "Black",
-                mainImage: "images/lambomacau1.jpg",
-                thumbnails: [
-                    "images/lambomacau2.jpg",
-                    "images/lambomacau3.jpg",
-                    "images/lambomacau4.jpg"
-                ],
-                sizes: ["1:64"]
-            }
-        ]
-    },
-    {
-        id: 14,
-        title: "Lamborghini Huracán GT3 EVO2 #83 Iron Dames 2023 IMSA Daytona 24 Hrs",
-        price: "450",
-        description: "MINI GT 1:64 Lamborghini Huracán GT3 EVO2 #83 Iron Dames 2023 IMSA Daytona 24 Hrs MGT00772",
-        colors: [
-            {
-                name: "Pink",
-                mainImage: "images/lambopink1.jpg",
-                thumbnails: [
-                    "images/lambopink2.jpg",
-                    "images/lambopink3.jpg",
-                    "images/lambopink4.jpg"
-                ],
-                sizes: ["1:64"]
-            }
-        ]
-    },
-    {
-        id: 15,
-        title: "Lamborghini Urus Performante Grigio Nimbus",
-        price: "500",
-        description: "MINI GT 1:64 Lamborghini Urus Performante Grigio Nimbus MGT00747",
-        colors: [
-            {
-                name: "Grey and Black",
-                mainImage: "images/uruslambo1.jpg",
-                thumbnails: [
-                    "images/urusblack2.jpg",
-                    "images/urusblack3.jpg",
-                    "images/urusblack4.jpg"
-                ],
-                sizes: ["1:64"]
-            }
-        ]
-    },
-    {
-        id: 166,
-        title: "McLaren Artura Ice Silver",
-        price: "400",
-        description: "MINI GT 1:64 McLaren Artura Ice Silver MGT00582",
-        colors: [
-            {
-                name: "Silver",
-                mainImage: "images/atura1.jpg",
-                thumbnails: [
-                    "images/atura2.jpg",
-                    "images/atura3.jpg",
-                    "images/atura4.jpg"
-                ],
-                sizes: ["1:64"]
-            }
-        ]
-    },
-   
-    
+    }
 ];
